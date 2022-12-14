71ebd2177196fdaa1bc44af730232cfcc5a0eec5
FahadAminShovon <29832989+FahadAminShovon@users.noreply.github.com>
2022-12-15 03:32:45 +0600

    fix(builder-bob): fix builder bob version

--- index.ts
+++
@@ -1,3 +0,0 @@
-export { CourierProvider } from './src/context';
-export { CourierScreen } from './src/screens/CourierScreen';
-export { BellIcon } from './src/components/BellIcon';
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export { CourierProvider } from './context';
+export { CourierScreen } from './screens/CourierScreen';
+export { BellIcon } from './components/BellIcon';
