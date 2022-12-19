4cb26bed32779515adb2e806c35f6c71fc92736f
FahadAminShovon <29832989+FahadAminShovon@users.noreply.github.com>
2022-12-20 04:04:50 +0600

    change Inbox color to black

--- src/screens/CourierScreen/CourierScreen.tsx
+++ src/screens/CourierScreen/CourierScreen.tsx
@@ -34,6 +34,7 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: SEMI_BOLD,
     marginRight: 8,
+    color: 'black',
   },
   overAll: {
     justifyContent: 'space-between',
